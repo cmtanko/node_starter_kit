8fe76555b2140fc866ba2674615995df6e4c70fe
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-11-13 15:03:29 +0545

    NSK-005 -m cleanups -t 10 min -s Completed

--- dist/bookshelf.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var bookshelfJs = require("bookshelf");
-var db_1 = require("./db");
-var bookshelf = bookshelfJs(db_1.default);
-bookshelf.plugin('bookshelf-manager');
-exports.default = bookshelf;
-//# sourceMappingURL=bookshelf.js.map
--- src/bookshelf.ts
+++
@@ -1,7 +0,0 @@
-import * as bookshelfJs from 'bookshelf';
-import knex from './db';
-const bookshelf = bookshelfJs(knex);
-
-bookshelf.plugin('bookshelf-manager');
-
-export default bookshelf;
