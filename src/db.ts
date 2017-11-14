8881a7e206d11dffbb4fda8c98a09e28b21250ac
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-11-14 17:25:37 +0545

    build

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,11 @@
 import * as knexJs from 'knex';
 import cfg = require('./knexfile');
 
-const db = knexJs(cfg.development);
+let db = null;
+if (process.env.NODE_ENV === 'production') {
+  db = knexJs(cfg.production);
+} else {
+  db = knexJs(cfg.development);
+}
 
 export default db;
