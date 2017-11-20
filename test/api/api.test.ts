26908aa6ab539bce882061995ea9ab7485f953b9
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-11-20 15:26:20 +0545

    test

--- test/api/api.test.ts
+++ test/api/api.test.ts
@@ -2,20 +2,17 @@ import {} from 'jest';
 import * as supertest from 'supertest';
 const env = process.env.NODE_ENV || 'development';
 
-let url = 'http://localhost:3000';
-if (env === 'production') {
-  url = process.env.APP_HOST;
-}
+let url ='https://node-starter-kit.herokuapp.com'
 const request = supertest(url);
 
 describe('API Test', () => {
   describe('GET /', () => {
-    it('should return 200 OK', done => {
-      request.get('/').expect(200, done);
+    it('should return 200 OK', (done) => {
+      request.get('/').expect(200,done);
     });
 
-    it('should return 404 NF for random api hit', done => {
-      request.get('/randomLinkUrl').expect(404, done);
+    it('should return 404 NF for random api hit', (done) => {
+      request.get('/randomLinkUrl').expect(404,done);
     });
   });
 });
